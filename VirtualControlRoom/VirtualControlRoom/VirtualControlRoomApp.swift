9ae3d75d8941090939c6508236906c896f60d847
alexhessler <alexhessler@gmail.com>
2025-06-01 19:45:39 -0700

    Adding Hello World
    adding Hello World

---
+++ VirtualControlRoom/VirtualControlRoom/VirtualControlRoomApp.swift
@@ -0,0 +1,33 @@
+//
+//  VirtualControlRoomApp.swift
+//  VirtualControlRoom
+//
+//  Created by Alex Hessler on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct VirtualControlRoomApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ VirtualControlRoom/VirtualControlRoomTests/VirtualControlRoomTests.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualControlRoomTests.swift
+//  VirtualControlRoomTests
+//
+//  Created by Alex Hessler on 6/1/25.
+//
+
+import Testing
+@testable import VirtualControlRoom
+
+struct VirtualControlRoomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
